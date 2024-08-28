d113139953cb8d32d8679554e9adf5d791a2976f
kdara20 <58341291+kdara20@users.noreply.github.com>
2024-08-28 00:44:25 -0400

    implement MobileNav

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
 import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
 
 export default function RootLayout({
   children,
@@ -8,10 +10,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   const loggedIn = { firstName: "Kevin", lastName: "Dara" };
+
   return (
-    <main className="flex h-screen w- font-inter">
+    <main className="flex h-screen w-full font-inter">
       <Sidebar user={loggedIn} />
-      {children}
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
     </main>
   );
 }
