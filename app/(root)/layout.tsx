f354623a65855a135bf825e119c88d51217b1d15
kdara20 <dcjay22@gmail.com>
2024-08-30 12:28:59 -0400

    Save from another computer

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import Sidebar from "@/components/Sidebar";
+import Image from "next/image";
+import MobileNav from "@/components/MobileNav";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  const loggedIn = { firstName: "Kevin", lastName: "Dara" };
+
+  return (
+    <main className="flex h-screen w-full font-inter">
+      <Sidebar user={loggedIn} />
+
+      <div className="flex size-full flex-col">
+        <div className="root-layout">
+          <Image src="/icons/logo.svg" width={30} height={30} alt="logo" />
+          <div>
+            <MobileNav user={loggedIn} />
+          </div>
+        </div>
+        {children}
+      </div>
+    </main>
+  );
+}
